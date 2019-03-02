ccaf30f15aeaa7300cf1b761caaf606c7cfc1f72
Emil <e.karamihov@student.fontys.nl>
2019-03-02 02:16:57 +0100

    first commit

---
+++ FormPaint/FormPaint/DrawingForm.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace FormPaint
+{
+    partial class DrawingForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnClear = new System.Windows.Forms.Button();
+            this.cordinatesLb = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BtnClear
+            // 
+            this.BtnClear.Location = new System.Drawing.Point(133, 396);
+            this.BtnClear.Name = "BtnClear";
+            this.BtnClear.Size = new System.Drawing.Size(75, 23);
+            this.BtnClear.TabIndex = 1;
+            this.BtnClear.Text = "Clear";
+            this.BtnClear.UseVisualStyleBackColor = true;
+            this.BtnClear.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // cordinatesLb
+            // 
+            this.cordinatesLb.AutoSize = true;
+            this.cordinatesLb.Location = new System.Drawing.Point(257, 401);
+            this.cordinatesLb.Name = "cordinatesLb";
+            this.cordinatesLb.Size = new System.Drawing.Size(60, 13);
+            this.cordinatesLb.TabIndex = 2;
+            this.cordinatesLb.Text = "Cordinates:";
+            // 
+            // DrawingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cordinatesLb);
+            this.Controls.Add(this.BtnClear);
+            this.Name = "DrawingForm";
+            this.Text = "Drawing Form";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.DrawingForm_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.DrawingForm_MouseClick);
+            this.Resize += new System.EventHandler(this.DrawingForm_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button BtnClear;
+        private System.Windows.Forms.Label cordinatesLb;
+    }
+}
+
---
+++ FormPaint/FormPaint/DrawingForm.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormPaint
+{
+    public partial class DrawingForm : Form
+    {
+        private List<Point> points;
+
+        public DrawingForm()
+        {
+            InitializeComponent();
+            points = new List<Point>();
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            Graphics gr = CreateGraphics();
+            gr.Clear(SystemColors.Control);
+            points.Clear();
+        }
+
+        private void DrawingForm_Paint(object sender, PaintEventArgs e)
+        {
+            float formX = ClientRectangle.Width - AutoScaleDimensions.Width;
+            float formY = ClientRectangle.Height - AutoScaleFactor.Height;
+            Graphics gr = e.Graphics;
+            DrawEllipses(gr, formX, formY, 75);
+            DrawLines(gr, formX, formY);
+            DrawDots(gr, 10);
+        }
+
+        private void DrawingForm_MouseClick(object sender, MouseEventArgs e)
+        {
+            cordinatesLb.Text = string.Format("Cordinates: {0}:{1}", e.X, e.Y);
+            points.Add(e.Location);
+            Refresh();
+        }
+
+        private void DrawingForm_Resize(object sender, EventArgs e)
+        {
+            Invalidate();
+        }
+
+        private void DrawEllipses(Graphics gr, float formX, float formY, int r)
+        {
+            float circleX = formX - r;
+            float circleY = formY - r;
+            gr.DrawEllipse(Pens.Blue, 0, 0, r, r);
+            gr.DrawEllipse(Pens.Red, circleX, 0, r, r);
+            gr.DrawEllipse(Pens.Purple, 0, circleY, r, r);
+            gr.DrawEllipse(Pens.Green, circleX, circleY, r, r);
+            gr.DrawEllipse(Pens.Aqua, circleX / 2, circleY / 2, r, r);
+        }
+
+        private void DrawLines(Graphics gr, float formX, float formY)
+        {
+            gr.DrawLine(Pens.Black, 0, 0, formX, formY);
+            gr.DrawLine(Pens.Black, 0, formY, formX, 0);
+        }
+
+        private void DrawDots(Graphics gr, int r)
+        {
+            int radiusCenter = r / 2;
+            if (points.Count > 1)
+            {
+                foreach (Point item in points)
+                    gr.FillEllipse(Brushes.Black, item.X - radiusCenter, item.Y - radiusCenter, r, r);
+                gr.DrawLines(Pens.Black, points.ToArray());
+            }
+            else if (points.Count == 1) gr.FillEllipse(Brushes.Black, points.Last().X - radiusCenter, points.Last().Y - radiusCenter, r, r);
+        }
+    }
+}
